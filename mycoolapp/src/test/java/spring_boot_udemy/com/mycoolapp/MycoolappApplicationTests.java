672e1efc07dc30b52b09fc1682fc6d05177c477c
Bol-Den <162582079+Bol-Den@users.noreply.github.com>
2024-07-17 10:25:39 +0300

    initial commit

---
+++ mycoolapp/src/test/java/spring_boot_udemy/com/mycoolapp/MycoolappApplicationTests.java
@@ -0,0 +1,13 @@
+package spring_boot_udemy.com.mycoolapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MycoolappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
