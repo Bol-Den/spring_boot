1910f80809dc6f652ef2954d54c73468e1c95526
Den <deniss.boltunovs@gmail.com>
2024-07-17 10:47:23 +0300

    rest controller added

---
+++ mycoolapp/src/main/java/spring_boot_udemy/com/mycoolapp/rest/FirstRestController.java
@@ -0,0 +1,16 @@
+package spring_boot_udemy.com.mycoolapp.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FirstRestController {
+
+    /// expose "/" to return Hello world!
+
+    @GetMapping("/")
+    public String helloWorld(){
+        return "Hello world!";
+    }
+
+}
