d77ca851910c2561db86ff375616b4d040488191
Den <deniss.boltunovs@gmail.com>
2024-07-18 17:54:55 +0300

    initial commit

---
+++ 01-spring-boot-overview/01-spring-boot-demo/src/main/java/spring_boot_udemy/com/mycoolapp/MycoolappApplication.java
@@ -0,0 +1,15 @@
+package spring_boot_udemy.com.mycoolapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MycoolappApplication {
+
+	public static void main(String[] args) {
+		
+		SpringApplication.run(MycoolappApplication.class, args);
+
+	}
+
+}
