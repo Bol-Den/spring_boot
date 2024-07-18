c26c50eeeb636064a7e291f6457a51274a28127d
Den <deniss.boltunovs@gmail.com>
2024-07-18 14:11:25 +0300

    adjsuted pom

--- mycoolapp/src/main/java/spring_boot_udemy/com/mycoolapp/rest/FirstRestController.java
+++ mycoolapp/src/main/java/spring_boot_udemy/com/mycoolapp/rest/FirstRestController.java
@@ -1,15 +1,22 @@
 package spring_boot_udemy.com.mycoolapp.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FirstRestController {
 
+    @Value("${coach.name}" )
+    private String coachName;
+
+    @Value("${team.name}")
+    private String teamName;
+
     /// expose "/" to return Hello world!
     @GetMapping("/")
     public String helloWorld(){
-        return "Hello world!";
+        return "Hello world $$$!";
     }
 
 }
